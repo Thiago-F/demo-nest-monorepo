0f14b7b180e4c2c11c7d4fa3b638e0cec920fd13
Thiago-F <thiago_fialho150@hotmail.com>
2022-11-13 18:45:12 -0300

    controller and service tests

--- apps/crud/src/app/products/controller/products.controller.spec.ts
+++ apps/crud/src/app/products/controller/products.controller.spec.ts
@@ -3,6 +3,7 @@ import { SaveProductDto } from '../dto/save-product.dto';
 import { ProductEntity } from '../entity/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductService } from '../service/products.service'
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 describe('ProductsController', () => {
   let productController: ProductsController;
@@ -15,7 +16,11 @@ describe('ProductsController', () => {
         {
           provide: ProductService,
           useValue: {
-            save: jest.fn()
+            save: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+            update: jest.fn(),
+            destroy: jest.fn(),
           }
         }
       ]
@@ -28,6 +33,38 @@ describe('ProductsController', () => {
   it('should be defined', () => {
     expect(productController).toBeDefined();
   });
+
+  describe('list and find', () => {
+    it('should list all products', async () => {
+      const mockProductEntity = {
+        name: 'any_product',
+        category: 'any_category',
+        value: 12345
+      } as ProductEntity
+
+      jest.spyOn(productService, 'findAll').mockResolvedValueOnce([mockProductEntity])
+
+      const result = await productController.findAll()
+
+      expect(result).toEqual([mockProductEntity])
+      expect(productService.findAll).toBeCalledTimes(1)
+    })
+
+    it('should find a product by id', async () => {
+      const mockProductEntity = {
+        name: 'any_product',
+        category: 'any_category',
+        value: 12345
+      } as ProductEntity
+
+      jest.spyOn(productService, 'findById').mockResolvedValueOnce(mockProductEntity)
+
+      const result = await productController.findById('any_id')
+
+      expect(result).toEqual(mockProductEntity)
+      expect(productService.findById).toBeCalledTimes(1)
+    })
+  })
 
   describe('save', () => {
     it('should create a new product on success', async () => {
@@ -47,6 +84,34 @@ describe('ProductsController', () => {
 
       expect(result).toBeDefined()
       expect(productService.save).toHaveBeenCalledWith(body)
+    })
+  })
+
+  describe('update', () => {
+    it('should update a product based on data passed', async () => {
+      const body: UpdateProductDto = {
+        name: 'any_product',
+        category: 'any_category',
+        value: 12345
+      }
+
+      const mockProductEntity = {
+        ...body
+      } as ProductEntity
+
+      jest.spyOn(productService, 'update').mockResolvedValueOnce(mockProductEntity)
+
+      const result = await productController.update('any_id', body)
+
+      expect(result).toBeDefined()
+      expect(productService.update).toHaveBeenCalledWith('any_id', body)
+    })
+  })
+
+  describe('delete', () => {
+    it('should delete a product by id', async () => {
+      await productController.destroy('any_id')
+      expect(productService.destroy).toBeCalledTimes(1)
     })
   })
 });
--- apps/crud/src/app/products/service/products.service.spec.ts
+++ apps/crud/src/app/products/service/products.service.spec.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { ProductService } from './products.service';
 import { ProductEntity } from '../entity/product.entity';
 import { SaveProductDto } from '../dto/save-product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 describe('ProductService', () => {
   let productService: ProductService;
@@ -18,6 +19,10 @@ describe('ProductService', () => {
           useValue: {
             create: jest.fn(),
             save: jest.fn(),
+            find: jest.fn(),
+            findOneOrFail: jest.fn(),
+            merge: jest.fn(),
+            softDelete: jest.fn(),
           }
         }
       ],
@@ -32,8 +37,48 @@ describe('ProductService', () => {
     expect(productRepository).toBeDefined();
   });
 
+  describe('list and find', () => {
+    it('should list all products', async () => {
+      const mockProductEntity = {
+        name: 'any_product',
+        category: 'any_category',
+        value: 12345
+      } as ProductEntity
+
+      jest.spyOn(productRepository, 'find').mockResolvedValueOnce([mockProductEntity])
+
+      const result = await productService.findAll()
+
+      expect(result).toEqual([mockProductEntity])
+      expect(productRepository.find).toBeCalledTimes(1)
+    })
+
+    it('should find a product by id', async () => {
+      const mockProductEntity = {
+        name: 'any_product',
+        category: 'any_category',
+        value: 12345
+      } as ProductEntity
+
+      jest.spyOn(productRepository, 'findOneOrFail').mockResolvedValueOnce(mockProductEntity)
+
+      const result = await productService.findById({ where: { id: 'any_id' } })
+
+      expect(result).toEqual(mockProductEntity)
+      expect(productRepository.findOneOrFail).toBeCalledTimes(1)
+    })
+
+    it('should throw if no product was found', async () => {
+      jest.spyOn(productRepository, 'findOneOrFail').mockImplementationOnce(() => { throw new Error('No product was found') })
+
+      const promise = productService.findById({ where: { id: 'any_id' } })
+
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
   describe('save', () => {
-    it('should call productRepository with correct values', async () => {
+    it('should save a productRepository.create with correct values', async () => {
       const data: SaveProductDto = {
         name: 'any_name',
         category: 'any_category',
@@ -52,6 +97,37 @@ describe('ProductService', () => {
       expect(result).toBeDefined()
       expect(productRepository.create).toBeCalledTimes(1)
       expect(productRepository.save).toBeCalledTimes(1)
+    })
+  })
+
+  describe('update', () => {
+    it('should call productRepository.update with correct values', async () => {
+      const data: UpdateProductDto = {
+        name: 'any_name',
+        category: 'any_category',
+        value: 12345,
+      }
+
+      const productEntityMock = {
+        ...data
+      } as ProductEntity
+
+      jest.spyOn(productRepository, 'findOneOrFail').mockResolvedValueOnce(productEntityMock)
+      jest.spyOn(productRepository, 'merge').mockReturnValueOnce(productEntityMock)
+      jest.spyOn(productRepository, 'save').mockResolvedValueOnce(productEntityMock)
+
+      const result = await productService.update('any_id', data)
+
+      expect(result).toBeDefined()
+      expect(productRepository.merge).toBeCalledTimes(1)
+      expect(productRepository.save).toBeCalledTimes(1)
+    })
+  })
+
+  describe('destroy', () => {
+    it('should call productRepository.destroy with correct values', async () => {
+      await productService.destroy('any_id')
+      expect(productRepository.softDelete).toBeCalledTimes(1)
     })
   })
 });
